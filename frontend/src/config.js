a1ee1095792bfbc75320cb52fec41c5e1a971b4f
Adicshan <rk8816616@gmail.com>
2026-02-03 20:27:24 +0530

    remove .env file

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 // Global API Configuration
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
+//const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = 'https://take-order.onrender.com';
 const API_URL = `${API_BASE_URL}/api`;
 
 export { API_BASE_URL, API_URL };
